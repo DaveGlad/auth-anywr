bec1b217e9b8506cb14aa42f3c196d6026d32531
Dave Glad <mdaveglad@gmail.com>
2023-04-26 10:02:42 +0100

    feat(styles): adding ant design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,18 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { store } from '@/src/store';
 import { Provider } from 'react-redux';
+import { StyleProvider } from '@ant-design/cssinjs';
+import { AntdThemeConfig } from '@/src/configs/antd.config';
+import { ConfigProvider } from 'antd';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ConfigProvider theme={AntdThemeConfig}>
+        <StyleProvider hashPriority="high">
+          <Component {...pageProps} />
+        </StyleProvider>
+      </ConfigProvider>
     </Provider>
   );
 }
---
+++ src/configs/antd.config.ts
@@ -0,0 +1,10 @@
+import { ThemeConfig } from 'antd/es/config-provider/context';
+
+export const AntdThemeConfig: ThemeConfig = {
+  token: {
+    colorPrimary: '#754ffe',
+    fontFamily: 'Inter',
+    colorLink: '#754ffe',
+    colorLinkHover: '#754ffe',
+  },
+};
